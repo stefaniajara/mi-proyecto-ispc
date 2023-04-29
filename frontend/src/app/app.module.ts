79fec4424570fccfb2e37aec761c990032e7dcdf
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-04-29 15:03:27 -0300

    Agrega componente navbar
    este será usado por el header, permite ingtegrar luego un componente de nav-mobile

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
+import { NavbarComponent } from './shared/layout/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
